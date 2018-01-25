9969a2221901338e3159bfa2102b20f72612e0be
Dmitry Ryadnenko <rongi@users.noreply.github.com>
2018-01-25 09:23:57 +0100

    Update MainViewModel.kt

--- app/src/main/java/com/github/rongi/rxpresenter/example/app/main/MainViewModel.kt
+++ app/src/main/java/com/github/rongi/rxpresenter/example/app/main/MainViewModel.kt
@@ -25,6 +25,8 @@ fun present(
 ): MainViewModel {
   // Internal states
 
+  // This emits [Start] item when request starts and [Result] item
+  // when request is completed
   val getArticlesWithStartAndEnd = articlesProvider.getArticles()
     .markStartAndEnd()
 
